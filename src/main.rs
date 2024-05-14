cd67dbd398cd731482293c35af3ef66d33d859b9
renatograsso10 <renatobueno95@hotmail.com>
2024-05-14 11:05:04 -0300

    Implemented HTTP response

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::net::TcpListener;
-
+use std::io::Write;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                let response = "HTTP/1.1 200 OK\r\n\r\n";
+                stream.write_all(response.as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
