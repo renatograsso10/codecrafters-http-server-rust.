ef92554d50c48c97ada595210d81e2813bc907d1
renatograsso10 <renatobueno95@hotmail.com>
2024-05-14 11:07:11 -0300

    Handle different URL paths and respond with 200 or 404 status

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::net::TcpListener;
-use std::io::Write;
+use std::io::{Read, Write};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -8,7 +8,17 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                let response = "HTTP/1.1 200 OK\r\n\r\n";
+                
+                let mut buffer = [0; 512];
+                stream.read(&mut buffer).unwrap();
+                let request = String::from_utf8_lossy(&buffer[..]);
+                
+                let response = if request.starts_with("GET / ") {
+                    "HTTP/1.1 200 OK\r\n\r\n"
+                } else {
+                    "HTTP/1.1 404 Not Found\r\n\r\n"
+                };
+                
                 stream.write_all(response.as_bytes()).unwrap();
             }
             Err(e) => {
@@ -16,4 +26,5 @@ fn main() {
             }
         }
     }
+}
 }
